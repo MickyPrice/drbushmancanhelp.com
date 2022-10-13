3dacbc48bb1545feefd1d0533eaecd2f684a4e9c
Michael Price <realmprice@gmail.com>
2022-10-13 21:07:34 +1300

    Add testimonial signatures

--- components/Testimonials.vue
+++ components/Testimonials.vue
@@ -4,21 +4,27 @@
       <p class="text-2xl mb-1">
         "My Rash is almost gone!"
       </p>
-      <span class="text-xl">~~~~~~~</span>
+      <div class="text-xl">
+        <img src="@/assets/img/testimonials/hovris_signature.png" alt="Horvis's Signature" class="h-6">
+      </div>
     </div>
 
     <div class="bg-gray-100 p-1 shadow-2xl flex flex-col justify-between">
       <p class="text-2xl mb-1">
         "He nearly cured my gum disease"
       </p>
-      <span class="text-xl">~~~~~~~</span>
+      <div class="text-xl">
+        <img src="@/assets/img/testimonials/groltons_signature.png" alt="Grolton's Signature" class="h-6">
+      </div>
     </div>
 
     <div class="bg-gray-100 p-1 shadow-2xl flex flex-col justify-between">
       <p class="text-2xl mb-1">
         "I don't even feel that sick now"
       </p>
-      <span class="text-xl">~~~~~~~</span>
+      <div class="text-xl">
+        <img src="@/assets/img/testimonials/davids_testimonial.png" alt="David's Signature" class="h-6">
+      </div>
     </div>
 
   </div>
